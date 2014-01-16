42843f411876eb5c4bc7cc759d2f12c676d58a62
Michel Blanc <mb@mbnet.fr>
2014-01-16 18:45:05 +0100

    Adds script and control files for .deb
    Added a bit more automation to build omxplayer's .deb

---
+++ build_omxplayer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please provide a GitHub clone URL as argument"
+  exit 1
+fi
+
+rm -rf omxplayer/
+git clone $1 omxplayer
+cp Makefile.include omxplayer/
+
+cd omxplayer
+
+make ffmpeg
+make
+make dist
+FORK=`git remote -v | cut -f2 -d':' | cut -f1 -d'/' | head -1`
+VERSION=0.3.4~git~$FORK~`date +%Y%m%d`~`git describe --always --tag`
+
+cd ..
+
+mkdir -p package/omxplayer/DEBIAN
+cp debian/{postrm,preinst,prerm} package/omxplayer/DEBIAN/
+sed -e "s/##VERSION##/$VERSION/" debian/control.tmpl > package/omxplayer/DEBIAN/control
+cp -aR omxplayer/omxplayer-dist/* package/omxplayer/
+dpkg-deb --build package/omxplayer .
+
